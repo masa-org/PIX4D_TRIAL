91292bb9bd5befb031a02ae65e4c3e837671fd86
Masatomo Ito <masatomo.ito@slalom.com>
2023-10-10 12:22:36 +0900

    create project

--- 1.get_bearer_token.sh
+++ 1.get_bearer_token.sh
@@ -2,6 +2,14 @@
 
 set -x
 
+. secrets.env
+
+if [ -z ${PIX4D_CLIENT_ID} ]
+then 
+  echo "CLIENT_ID not set"
+  exit
+fi
+
 curl -s --request POST \
   --url https://cloud.pix4d.com/oauth2/token/ \
   --form client_id=$PIX4D_CLIENT_ID \
--- 2.create_project.sh
+++ 2.create_project.sh
@@ -1,3 +1,4 @@
+
 #!/bin/bash
 
 set -x
@@ -8,11 +9,35 @@ PIX4D_ACCESS_TOKEN=$(cat access_token.txt)
 if [ -z "${PIX4D_ACCESS_TOKEN}" ]
   then
     echo "ACCESS_TOKEN is not set"
+    exit 1
 fi
 
-curl --request POST \
+BODY_PROJECT=$(curl -s --request POST \
   --url https://cloud.pix4d.com/project/api/v3/projects/ \
   --header "Authorization: Bearer ${PIX4D_ACCESS_TOKEN}" \
   --header "Content-Type: application/json" \
-  --data "{\"name\": \"${PROJECT_NAME}\"}"
+  --data "{\"name\": \"${PROJECT_NAME}\"}")
  
+PROJECT_ID=$(echo ${BODY_PROJECT} | jq -r '.id')
+
+if [ -z "${PROJECT_ID}" ]
+  then
+    echo "PROJECT_ID is not set"
+    exit 1
+fi
+
+BODY_AWS=$(curl -s --url https://cloud.pix4d.com/project/api/v3/projects/${PROJECT_ID}/s3_credentials/ \
+  --header "Authorization: Bearer ${PIX4D_ACCESS_TOKEN}" \
+  --header "Content-Type: application/json")
+
+echo $BODY_AWS| jq 
+
+tee project_setup.env <<EOT
+export PROJECT_ID=${PROJECT_ID}
+export S3_BUCKET_NAME=$(echo ${BODY_PROJECT} | jq -r '.bucket_name')
+export S3_BASE_PATH=$(echo ${BODY_PROJECT} | jq -r '.s3_base_path')
+export AWS_ACCESS_KEY_ID=$(echo ${BODY_AWS} | jq -r '.access_key')
+export AWS_SECRET_ACCESS_KEY=$(echo ${BODY_AWS} | jq -r '.secret_key')
+export AWS_SESSION_TOKEN=$(echo ${BODY_AWS} | jq -r '.session_token')
+export AWS_REGION=$(echo ${BODY_AWS} | jq -r '.region')
+EOT
